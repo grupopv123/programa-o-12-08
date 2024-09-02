995170f062fb051841c06a821cb37163ebe4d977
PAULOSPAK <102532032+grupopv123@users.noreply.github.com>
2024-09-02 09:57:23 -0300

    ..

--- script.js
+++ script.js
@@ -1,55 +1,91 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-
 const caixaResultado = document.querySelector(".caixa-resultado");
-
-const caixaResultado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-     enunciado: "A inteligência artificial (IA) é um conjunto de tecnologias que permitem aos computadores executar uma variedade de funções avançadas. Quais são as implicações da IA na privacidade dos dados?",
-       alternativas: [
-           "A IA, embora possa desempenhar um papel fundamental na detecção e prevenção de ameaças à segurança.",
-           "Também pode ser usada como uma ferramenta para a invasão de privacidade."
-       ]
-    },
-    
-    {
-     enunciado: "Inteligência artificial criando artistas 100% virtuais; músicas, obra de arte,incentivando o consumo não consciente  prejudicando a tradição e cultura. Como preservar a tradição e a cultura no atual contexto regido pelas inteligências artificiais",
-      alternativas: [
-           "Realizando pesquisas para saber sobre suas tradições e culturas.",
-            "Alternativa 2"
-       ]
+        enunciado: "Qual é a melhor maneira de construir um fundo de emergência?",
+        alternativas: [
+            {
+                texto: " Investir em ações de alto risco",
+                afirmacao: "Infelizmente não foi dessa vez"
+            },
+            {
+                texto: "Manter uma poupança separada com o equivalente a 3 a 6 meses de despesa",
+                afirmacao: "Correto, parabéns pelo conhecimento"
+            }
+        ]
     },
     {
-        enunciado: "Tomamos como referência que um consumo consciente é aquele do qual o indivíduo compra somente o necessário, Como a sociedade poderia ter um consumo conciente, Buscando informações no IA?",
-      alternativas: [
-               "Buscando informação na legislação da saúde",
-               "Confronto das informações da inteligengia artificial e a pessoal"
-       ]
+        enunciado: "Qual é uma estratégia eficaz para reduzir dívidas de cartão de crédito?",
+        alternativas: [
+            {
+                texto: "Fazer apenas o pagamento mínimo mensal",
+                afirmacao: "Infelizmente não foi dessa vez"
+            },
+            {
+                texto: "Pagar mais do que o mínimo e considerar a transferência para uma taxa de juros",
+                afirmacao: "Correto, parabéns pelo conhecimento"
+            }
+        ]
+    },
+    {
+        enunciado: "Qual é uma vantagem de diversificar seus investimentos?",
+        alternativas: [
+            {
+                texto: "Reduzir o risco total da carreira",
+                afirmacao: "Perfeito, parabéns"
+            },
+            {
+                texto: "Aumentar a chance de ganhar mais em investimentos de alto risco",
+                afirmacao: "Quase lá, não foi dessa vez"
+            }
+        ]
     }
-     ];
+   
+];
 
-     let atual = 0;
-     let perguntaAtual;
 
-function mostraPergunta(){
-     perguntaAtual = perguntas[atual];
-     caixaPerguntas.textContent = perguntaAtual.enunciado;
-     caixaAlternativas.textContent= "";
-     mostraAlternativas();
-    
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
     }
-function mostraAlternativas(){
-   for(const alternativa of perguntaAtual.alternativas){
-    const botaoAlternativas= document.creatElement("button");
-    botaoAlternativas.textContent = alternativa;
-    caixaAlternativas.appendChild(botaoAlternativas);
-
-   }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
 
-     mostraPergunta();
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacao +" ";
+    atual++;
+    mostraPergunta();
+}
+
+
+
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "IA mostra perfil de quem ajuda o meio ambiente!";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
